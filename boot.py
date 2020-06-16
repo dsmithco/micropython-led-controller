8e44b40ad7c93b862cd570a90c7dc5a74c437a61
Smith, Dan <Dan_Smith@intuit.com>
2020-06-15 23:52:07 -0700

    updating controller

--- boot.py
+++ boot.py
@@ -4,10 +4,21 @@ esp.osdebug(None)
 import uos, machine
 #uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
+import ujson
+
 #import webrepl
 #webrepl.start()
 gc.collect()
 
 ap = network.WLAN(network.AP_IF)
 ap.active(True)
-ap.config(essid='ReThinkLED', password='Rethink44')
+
+try:
+    f = open('data.json', "r")
+    data = ujson.loads(f.read())
+    f.close()
+    ap.config(essid=data['ssid'], password=data['password'])
+except:
+    ap.config(essid='ReThinkLED', password='Rethink')
+
+
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import ujson
 def get_data(data_file='data.json'):
     f = open(data_file, "r")
     data = ujson.loads(f.read())
+    f.close()
     return data
 
 data = get_data()
@@ -17,7 +18,7 @@ async def startup():
 
 def set_color(step1_color_arr):
     for n in range(np.n):
-        np[n] = (step1_color_arr[0], step1_color_arr[1], step1_color_arr[2])
+        np[n] = (int(step1_color_arr[0]), int(step1_color_arr[1]), int(step1_color_arr[2]))
     np.write()
 
 async def run_steps(data, count=0):
@@ -26,6 +27,15 @@ async def run_steps(data, count=0):
         steps = data['steps']
         for i, step in enumerate(steps):
 
+            this_colors = []
+            try:
+                color = step['color'].replace('#','')
+                this_colors.append(eval('0x'+color[0:-5]+color[1:-4]))
+                this_colors.append(eval('0x'+color[2:-3]+color[3:-2]))
+                this_colors.append(eval('0x'+color[4:-1]+color[5:]))
+            except:
+                this_colors = [40,40,40]
+
             last = len(steps) == i+1
 
             if last:
@@ -33,8 +43,19 @@ async def run_steps(data, count=0):
             else:
                 next_step = steps[i+1]
 
+            color = next_step['color'].replace('#','')
+            next_colors = []
+            try:
+                next_colors.append(eval('0x'+color[0:-5]+color[1:-4]))
+                next_colors.append(eval('0x'+color[2:-3]+color[3:-2]))
+                next_colors.append(eval('0x'+color[4:-1]+color[5:]))
+            except:
+                next_colors = [90,90,90]
+
+
+
             # Loop through the colors in the array of rgb colors
-            set_color(step['color'])
+            set_color(this_colors)
 
             # Do the step pause
             await asyncio.sleep_ms(step['pause_ms'])
@@ -43,19 +64,20 @@ async def run_steps(data, count=0):
             if count == 0:
                 await asyncio.sleep_ms(data['initial_pause_ms'])
 
-            from_g = step['color'][0]
-            from_r = step['color'][1]
-            from_b = step['color'][2]
+            from_g = int(this_colors[0])
+            from_r = int(this_colors[1])
+            from_b = int(this_colors[2])
 
-            to_g = next_step['color'][0]
-            to_r = next_step['color'][1]
-            to_b = next_step['color'][2]
+            to_g = int(next_colors[0])
+            to_r = int(next_colors[1])
+            to_b = int(next_colors[2])
             
             frames = 30
             frames_delay = int(step['transition_ms']//frames) or 1
             for r in range(1, frames):
                 set_color([from_g + (r * (to_g - from_g)//frames), from_r + (r * (to_r - from_r)//frames), from_b + (r * (to_b - from_b)//frames)])
                 await asyncio.sleep_ms(frames_delay)
+
             count += 1
 
 
@@ -74,27 +96,74 @@ async def toggle_onboard_led(state='on'):
 
 
 @asyncio.coroutine
-async def serve(reader, writer):
-    print(reader, writer)
-    print("================")
-    # print((yield from reader.read()))
+def serve(reader, writer):
+    r = yield from reader.read()
+    res = r.decode('latin-1')
 
-    res = yield from reader.read()
-    res = str(res).replace("b'","")
-    res = str(res).replace("'","")
-    resArr = res.split(' ')
-    method = resArr[0]
-    path = resArr[1]
-    yield from writer.awrite("HTTP/1.0 200 OK\r\n\r\nHello You. " + method + " " + str(path) + "\r\n")
-    print("After response write")
-    yield from writer.aclose()
-    print("Finished processing request")
+    # reader.reset()
+    if 'POST /' in res:
+        error = False
+        # f = open('index.html', "r")
+        # content = f.read()
+        # f.close()
+        data = res.split('\n')[-1]
+
+        # print('data', data)
+        try:
+            res_json = ujson.loads(data)
+            if 'steps' not in res_json:
+                error = True
+        except:
+            error = True
+            res_json = "{}"
+
+        if error == False:
+            f = open('data.json', "w")
+            f.write(ujson.dumps(res_json))
+            f.close()
+            machine.reset()
+
+        yield from writer.awrite("HTTP/1.0 201 OK\r\n\r\n" + ujson.dumps(res_json) + "\r\n")
+        yield from writer.aclose()
+        return
+
+    if 'GET /data.json ' in res:
+        f = open('data.json', "r")
+        content = f.read()
+        f.close()
+        yield from writer.awrite("HTTP/1.0 200 OK\r\n\r\n" + content + "\r\n")
+        yield from writer.aclose()
+        return
+
+    if 'GET /min.css ' in res:
+        f = open('min.css', "r")
+        content = f.read()
+        f.close()
+        yield from writer.awrite("HTTP/1.0 200 OK\r\n\r\n" + content + "\r\n")
+        yield from writer.aclose()
+        return
+
+    if 'GET /main.js ' in res:
+        f = open('main.js', "r")
+        content = f.read()
+        f.close()
+        yield from writer.awrite("HTTP/1.0 200 OK\r\n\r\n" + content + "\r\n")
+        yield from writer.aclose()
+        return
+
+    if 'GET / ' in res:
+        f = open('index.html', "r")
+        content = f.read()
+        f.close()
+        yield from writer.awrite("HTTP/1.0 200 OK\r\n\r\n" + content + "\r\n")
+        yield from writer.aclose()
+        return
 
 
 loop = asyncio.get_event_loop()
 loop.create_task(startup())
 loop.create_task(toggle_onboard_led())
-loop.create_task(asyncio.start_server(serve, "0.0.0.0", 80))
+loop.create_task(asyncio.start_server(serve, "0.0.0.0", 80, 10))
 loop.run_forever()
 loop.close()
 
